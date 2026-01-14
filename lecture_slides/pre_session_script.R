5ef9636ebd4aef448986067ec021dd7d5148e7f6
voigtstefan <stefan.voigt@econ.ku.dk>
2026-01-14 14:42:02 +0100

    Update repository

--- lecture_slides/pre_session_script.R
+++ lecture_slides/pre_session_script.R
@@ -7,11 +7,12 @@ library(kableExtra)
 
 theme_set(theme_bw() + theme(legend.position = "bottom"))
 
-
-def.chunk.hook  <- knitr::knit_hooks$get("chunk")
+def.chunk.hook <- knitr::knit_hooks$get("chunk")
 knitr::knit_hooks$set(chunk = function(x, options) {
   x <- def.chunk.hook(x, options)
-  ifelse(options$size != "normalsize", paste0("\n \\", options$size,"\n\n", x, "\n\n \\normalsize"), x)
+  ifelse(
+    options$size != "normalsize",
+    paste0("\n \\", options$size, "\n\n", x, "\n\n \\normalsize"),
+    x
+  )
 })
-
-
